deaa4080c0b928fd0a0062915248b63f58f4e836
Dmitry <131748949+SimkaGame@users.noreply.github.com>
2024-12-14 00:28:14 +0300

    upgrade

--- Completed Site/Bars/sketch.js
+++ Completed Site/Bars/sketch.js
@@ -8,8 +8,8 @@ function preload() {
 function setup() {
     createCanvas(800, 800);
     
-    myBar = new Bar(50, 50, 700, 700); // Увеличен отступ для текста
-    myBar.setupAxis(0, 5, 0, 100, 1, 10); // Настроены границы осей
+    myBar = new Bar(30, 30, 700, 700);
+    myBar.setupAxis(0, 10, 0, 150, 1, 10);
 
     let sums = {
         quiz: 0,
@@ -17,28 +17,95 @@ function setup() {
         exam: 0,
         finalMark: 0,
     };
+    let counts = {
+        quiz: 0,
+        project: 0,
+        exam: 0,
+        finalMark: 0,
+    };
+    let genderCounts = {
+        Male: 0,
+        Female: 0
+    };
+    let maxValues = {
+        quiz: 0,
+        project: 0,
+        exam: 0,
+        finalMark: 0
+    };
 
-    // Подсчет суммы
+    // Сумма и подсчеты
     for (let i = 0; i < studentData.getRowCount(); i++) {
         sums.quiz += studentData.getNum(i, 'quiz');
         sums.project += studentData.getNum(i, 'project');
         sums.exam += studentData.getNum(i, 'exam');
         sums.finalMark += studentData.getNum(i, 'finalMark');
+        counts.quiz++;
+        counts.project++;
+        counts.exam++;
+        counts.finalMark++;
+
+        // Подсчет количества по гендеру
+        let gender = studentData.getString(i, 'gender');
+        if (gender === 'M') {
+            genderCounts.Male++;
+        } else if (gender === 'F') {
+            genderCounts.Female++;
+        }
+
+        // Поиск максимальных значений
+        let quiz = studentData.getNum(i, 'quiz');
+        let project = studentData.getNum(i, 'project');
+        let exam = studentData.getNum(i, 'exam');
+        let finalMark = studentData.getNum(i, 'finalMark');
+
+        if (quiz > maxValues.quiz) {
+            maxValues.quiz = quiz;
+        }
+        if (project > maxValues.project) {
+            maxValues.project = project;
+        }
+        if (exam > maxValues.exam) {
+            maxValues.exam = exam;
+        }
+        if (finalMark > maxValues.finalMark) {
+            maxValues.finalMark = finalMark;
+        }
     }
 
-    // Средние значения
+    // Среднее
     let averages = {
-        quiz: sums.quiz / studentData.getRowCount(),
-        project: sums.project / studentData.getRowCount(),
-        exam: sums.exam / studentData.getRowCount(),
-        finalMark: sums.finalMark / studentData.getRowCount(),
+        quiz: sums.quiz / counts.quiz,
+        project: sums.project / counts.project,
+        exam: sums.exam / counts.exam,
+        finalMark: sums.finalMark / counts.finalMark,
     };
 
     // Добавление столбцов в диаграмму
-    myBar.addBar(1, averages.quiz, 0.5, 'red');
-    myBar.addBar(2, averages.project, 0.5, 'blue');
-    myBar.addBar(3, averages.exam, 0.5, 'green');
-    myBar.addBar(4, averages.finalMark, 0.5, 'orange');
+    myBar.addBar(1, averages.quiz, 1, 'red');
+    myBar.addLabel(1 + 0.5, averages.quiz + 5, 'red', averages.quiz.toFixed(2)); // Отображение среднего значения в формате числа
+    myBar.addBar(2, averages.project, 1, 'blue');
+    myBar.addLabel(2 + 0.5, averages.project  + 5, 'blue', averages.project.toFixed(2)); // Отображение среднего значения в формате числа
+    myBar.addBar(3, averages.exam, 1, 'green');
+    myBar.addLabel(3 + 0.5, averages.exam  + 5, 'green', averages.exam.toFixed(2)); // Отображение среднего значения в формате числа
+    myBar.addBar(4, averages.finalMark, 1, 'orange');
+    myBar.addLabel(4 + 0.5, averages.finalMark  + 5, 'orange', averages.finalMark.toFixed(2)); // Отображение среднего значения в формате числа
+
+    // Добавление столбцов для максимальных значений
+    myBar.addBar(5, maxValues.quiz, 1, 'darkred');
+    myBar.addLabel(5 + 0.5, maxValues.quiz  + 5, 'darkred', maxValues.quiz);
+    myBar.addBar(6, maxValues.project, 1, 'darkblue');
+    myBar.addLabel(6 + 0.5, maxValues.project  + 5, 'darkblue', maxValues.project);
+    myBar.addBar(7, maxValues.exam, 1, 'darkgreen');
+    myBar.addLabel(7 + 0.5, maxValues.exam  + 5, 'darkgreen', maxValues.exam);
+    myBar.addBar(8, maxValues.finalMark, 1, 'darkorange');
+    myBar.addLabel(8 + 0.5, maxValues.finalMark  + 5, 'darkorange', maxValues.finalMark);
+
+    // Добавление столбцов для количества по гендеру
+    myBar.addBar(9, genderCounts.Male, 1, 'lightblue');
+    myBar.addLabel(9 + 0.5, genderCounts.Male  + 5, 'lightblue', genderCounts.Male + ' Male');
+    myBar.addBar(10, genderCounts.Female, 1, 'pink');
+    myBar.addLabel(10 + 0.5, genderCounts.Female  + 5, 'pink', genderCounts.Female + ' Female');
 }
 
 function draw() {
@@ -61,38 +128,36 @@ function Bar(x, y, width, height) {
         strokeWeight(1);
         fill(0);
         
-        // Оси
         line(this.x, this.y + this.height, this.x + this.width, this.y + this.height); // X ось
         line(this.x, this.y, this.x, this.y + this.height); // Y ось
         
-        // Шкала X
+
         for (let i = xMin; i <= xMax; i++) {
-            let xPos = this.x + (i - xMin) * (this.width / (xMax - xMin));
-            line(xPos, this.y + this.height, xPos, this.y + this.height + 5);
-            textAlign(CENTER);
-            text(i, xPos, this.y + this.height + 15);
+            line(this.x + i * (this.width / (xMax - xMin)), this.y + this.height, this.x + i * (this.width / (xMax - xMin)), this.y + this.height + 5);
+            text(i, this.x + i * (this.width / (xMax - xMin)), this.y + this.height + 15);
         }
 
-        // Шкала Y
+
         for (let j = yMin; j <= yMax; j += yTickSpacing) {
-            let yPos = this.y + this.height - (j - yMin) * (this.height / (yMax - yMin));
-            line(this.x, yPos, this.x - 5, yPos);
-            textAlign(RIGHT, CENTER);
-            text(j, this.x - 10, yPos);
+            line(this.x, this.y + this.height - j * (this.height / yMax), this.x - 5, this.y + this.height - j * (this.height / yMax));
+            text(j, this.x - 25, this.y + this.height - j * (this.height / yMax));
         }
     };
 
-    this.addBar = function (xValue, yValue, barWidth, colour) {
+    this.addBar = function (xValue, yValue, width, colour) {
         if (yValue >= this.yMin && yValue <= this.yMax) {
             fill(colour);
-            noStroke();
-            
-            let barX = this.x + (xValue - this.xMin) * (this.width / (this.xMax - this.xMin));
-            let barY = this.y + this.height - (yValue - this.yMin) * (this.height / (this.yMax - this.yMin));
-            let barHeight = (yValue - this.yMin) * (this.height / (this.yMax - this.yMin));
-            let barW = barWidth * (this.width / (this.xMax - this.xMin));
-            
-            rect(barX - barW / 2, barY, barW, barHeight);
+            strokeWeight(4);
+            stroke(0);
+            rect(this.x + xValue * (this.width / (this.xMax - this.xMin)) - width / 2, this.y + this.height - yValue * (this.height / this.yMax), width * (this.width / (this.xMax - this.xMin)), yValue * (this.height / this.yMax));
         }
+    };
+
+    this.addLabel = function (xValue, yValue, colour, label = '') {
+        fill(colour);
+        noStroke();
+        textSize(12);
+        textAlign(CENTER, BOTTOM);
+        text(label || yValue, this.x + xValue * (this.width / (this.xMax - this.xMin)), this.y + this.height - yValue * (this.height / this.yMax) - 5);
     };
 }
